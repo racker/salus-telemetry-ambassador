e8e9f9563dae7626b55f394fdc5e85ab508a1e19
Geoff Bourne <geoff.bourne@rackspace.com>
2019-01-14 16:03:17 -0600

    Produce AttachEvent to Kafka on Envoy attachment

---
+++ src/test/java/com/rackspace/salus/telemetry/ambassador/services/EnvoyRegistryTest.java
@@ -0,0 +1,105 @@
+package com.rackspace.salus.telemetry.ambassador.services;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.rackspace.salus.services.TelemetryEdge.EnvoyInstruction;
+import com.rackspace.salus.services.TelemetryEdge.EnvoySummary;
+import com.rackspace.salus.telemetry.ambassador.config.AmbassadorProperties;
+import com.rackspace.salus.telemetry.ambassador.config.GrpcConfig;
+import com.rackspace.salus.telemetry.etcd.EtcdUtils;
+import com.rackspace.salus.telemetry.etcd.services.EnvoyLabelManagement;
+import com.rackspace.salus.telemetry.etcd.services.EnvoyLeaseTracking;
+import com.rackspace.salus.telemetry.etcd.services.EnvoyResourceManagement;
+import com.rackspace.salus.telemetry.events.AttachEvent;
+import com.rackspace.salus.telemetry.model.ResourceInfo;
+import io.grpc.StatusException;
+import io.grpc.stub.StreamObserver;
+import java.net.InetSocketAddress;
+import java.util.Collections;
+import java.util.concurrent.CompletableFuture;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+    webEnvironment = WebEnvironment.NONE,
+    classes = {
+        EnvoyRegistry.class, AmbassadorProperties.class,
+        LabelRulesProcessor.class,
+        GrpcConfig.class
+    }
+)
+@JsonTest
+public class EnvoyRegistryTest {
+
+  @MockBean
+  EnvoyLabelManagement envoyLabelManagement;
+
+  @MockBean
+  EnvoyLeaseTracking envoyLeaseTracking;
+
+  @MockBean
+  EnvoyResourceManagement envoyResourceManagement;
+
+  @MockBean
+  KafkaTemplate kafkaTemplate;
+
+  @Mock
+  StreamObserver<EnvoyInstruction> streamObserver;
+
+  @Autowired
+  EnvoyRegistry envoyRegistry;
+
+  @Test
+  public void postsAttachEventOnAttach() throws StatusException {
+    final EnvoySummary envoySummary = EnvoySummary.newBuilder()
+        .setIdentifierName("hostname")
+        .putLabels("hostname", "test-host")
+        .build();
+
+    final CompletableFuture<Long> assignedLease = CompletableFuture.completedFuture(1234L);
+    when(envoyLeaseTracking.grant(any()))
+        .thenReturn(assignedLease);
+
+    // using doReturn since return type is a wildcard capture
+    when(envoyLabelManagement.registerAndSpreadEnvoy(any(), any(), any(), anyLong(), any(), any()))
+        .then(invocationOnMock -> EtcdUtils.completedPutResponse());
+
+    when(envoyLabelManagement.pullAgentInstallsForEnvoy(any(), any(), any(), any(), any()))
+        .thenReturn(CompletableFuture.completedFuture(0));
+
+    when(envoyLabelManagement.pullConfigsForEnvoy(any(), any(), any(), any(), any()))
+        .thenReturn(CompletableFuture.completedFuture(0));
+
+    when(envoyResourceManagement.registerResource(any(), any(), anyLong(), any(), any(), any()))
+        .thenReturn(CompletableFuture.completedFuture(new ResourceInfo()));
+
+    envoyRegistry.attach("t-1", "e-1", envoySummary,
+        InetSocketAddress.createUnresolved("localhost", 60000), streamObserver
+    ).join();
+
+    verify(kafkaTemplate)
+        .send(
+            "telemetry.event.resource.json",
+            "t-1:hostname:test-host",
+            new AttachEvent()
+                .setIdentifierName("hostname")
+                .setIdentifierValue("test-host")
+                .setLabels(Collections.singletonMap("hostname", "test-host"))
+                .setEnvoyId("e-1")
+                .setTenantId("t-1")
+                .setEnvoyAddress("localhost")
+        );
+  }
+}
