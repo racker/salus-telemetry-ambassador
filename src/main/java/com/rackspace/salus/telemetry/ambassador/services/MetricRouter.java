99036c8d825e7a6e85cf42a31a14ece17adaf785
Geoff Bourne <geoff.bourne@rackspace.com>
2019-07-01 15:58:34 -0500

    Better logging and handling when lingering Envoys send metrics (#47)
    * Better logging and handling when lingering Envoys send metrics
    
    * Catch NPEs from avro handling of string fields expected to be non-null

--- src/main/java/com/rackspace/salus/telemetry/ambassador/services/MetricRouter.java
+++ src/main/java/com/rackspace/salus/telemetry/ambassador/services/MetricRouter.java
@@ -1,19 +1,17 @@
 /*
- *    Copyright 2018 Rackspace US, Inc.
+ * Copyright 2019 Rackspace US, Inc.
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- *
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.rackspace.salus.telemetry.ambassador.services;
@@ -90,6 +88,13 @@ public class MetricRouter {
         String resourceId = tagsMap.remove(BoundMonitorUtils.LABEL_RESOURCE);
         if (resourceId == null) {
             resourceId = envoyRegistry.getResourceId(envoyId);
+
+            if (resourceId == null) {
+                log.warn("Unable to locate resourceId while routing"
+                        + " measurement={} for tenant={} envoy={} tags={}",
+                    nameTagValue.getName(), tenantId, envoyId, nameTagValue.getTagsMap());
+                return;
+            }
         }
 
         final String taggedTargetTenant = tagsMap.remove(BoundMonitorUtils.LABEL_TARGET_TENANT);
@@ -135,7 +140,7 @@ public class MetricRouter {
             metricsRouted.increment();
             kafkaEgress.send(tenantId, KafkaMessageType.METRIC, out.toString(StandardCharsets.UTF_8.name()));
 
-        } catch (IOException e) {
+        } catch (IOException|NullPointerException e) {
             log.warn("Failed to Avro encode avroMetric={} original={}", externalMetric, postedMetric, e);
             throw new RuntimeException("Failed to Avro encode metric", e);
         }
--- src/test/java/com/rackspace/salus/telemetry/ambassador/services/MetricRouterTest.java
+++ src/test/java/com/rackspace/salus/telemetry/ambassador/services/MetricRouterTest.java
@@ -1,19 +1,17 @@
 /*
- *    Copyright 2018 Rackspace US, Inc.
+ * Copyright 2019 Rackspace US, Inc.
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- *
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.rackspace.salus.telemetry.ambassador.services;
@@ -138,6 +136,37 @@ public class MetricRouterTest {
         verify(kafkaEgress).send("t-some-other", KafkaMessageType.METRIC,
             readContent("/MetricRouterTest/testRouteMetric_withTargetTenant.json"));
 
+        verifyNoMoreInteractions(kafkaEgress, envoyRegistry);
+    }
+
+    @Test
+    public void testRouteMetric_invalid_noResourceId() {
+        Map<String, String> envoyLabels = new HashMap<>();
+        envoyLabels.put("hostname", "host1");
+        envoyLabels.put("os", "linux");
+
+        when(resourceLabelsService.getResourceLabels(any(), any()))
+            .thenReturn(envoyLabels);
+
+        when(envoyRegistry.getResourceId(any()))
+            // induce the scenario
+            .thenReturn(null);
+
+        final TelemetryEdge.PostedMetric postedMetric = TelemetryEdge.PostedMetric.newBuilder()
+            .setMetric(TelemetryEdge.Metric.newBuilder()
+                .setNameTagValue(TelemetryEdge.NameTagValueMetric.newBuilder()
+                    .setTimestamp(1539030613123L)
+                    .setName("cpu")
+                    .putTags("cpu", "cpu1")
+                    .putFvalues("usage", 1.45)
+                    .putSvalues("status", "enabled")
+                    .build())
+            )
+            .build();
+
+        metricRouter.route("t-1", "e-1", postedMetric);
+
+        verify(envoyRegistry).getResourceId("e-1");
         verifyNoMoreInteractions(kafkaEgress, envoyRegistry);
     }
 
