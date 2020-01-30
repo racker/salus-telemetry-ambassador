bbf2a16a0ea918ffc2b35911b27e712aaab2b3ad
Geoff Bourne <geoff.bourne@rackspace.com>
2020-01-30 13:25:37 -0600

    Added resourceId and measurementName to kafka metrics routing key

--- src/main/java/com/rackspace/salus/telemetry/ambassador/services/LogEventRouter.java
+++ src/main/java/com/rackspace/salus/telemetry/ambassador/services/LogEventRouter.java
@@ -1,19 +1,17 @@
 /*
- *    Copyright 2018 Rackspace US, Inc.
+ * Copyright 2020 Rackspace US, Inc.
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- *
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.rackspace.salus.telemetry.ambassador.services;
@@ -26,8 +24,10 @@ import com.rackspace.salus.services.TelemetryEdge.LogEvent;
 import com.rackspace.salus.telemetry.messaging.KafkaMessageType;
 import java.io.IOException;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -89,7 +89,8 @@ public class LogEventRouter {
                 final String enrichedJson = objectMapper.writeValueAsString(eventObj);
 
                 log.trace("Sending: {}", enrichedJson);
-                kafkaEgress.send(tenantId, KafkaMessageType.LOG, enrichedJson);
+                kafkaEgress.send(Strings.join(List.of(tenantId, resourceId), ','),
+                    KafkaMessageType.LOG, enrichedJson);
             }
             else {
                 log.warn("Unexpected json tree type: {}", event);
