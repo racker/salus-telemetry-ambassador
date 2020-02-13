aab4a2c132058da4ca16f59a22b36e8c623b4b19
Adam Martinek <harageth@users.noreply.github.com>
2020-02-13 12:33:04 -0800

    Adam9500/validate resource (#77)
    * added resourceId validation and tests
    
    * removed some extraneous comments
    
    * updated the tests to validate the exception thrown
    
    * altered test to use assertThatThrownBy
    
    * made the pattern a private static field

--- src/main/java/com/rackspace/salus/telemetry/ambassador/services/EnvoyRegistry.java
+++ src/main/java/com/rackspace/salus/telemetry/ambassador/services/EnvoyRegistry.java
@@ -85,6 +85,8 @@ public class EnvoyRegistry {
   private ConcurrentHashMap<String, EnvoyEntry> envoys = new ConcurrentHashMap<>();
   private ConcurrentHashMap<String, EnvoyEntry> envoysByResourceId = new ConcurrentHashMap<>();
 
+  private static final Pattern resourceValidation = Pattern.compile("[A-Za-z0-9:-]+");
+
   @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
   @Autowired
   public EnvoyRegistry(AmbassadorProperties appProperties,
@@ -156,7 +158,7 @@ public class EnvoyRegistry {
         envoySummary.getSupportedAgentsList());
 
     final String resourceId = envoySummary.getResourceId();
-    final Pattern resourceValidation = Pattern.compile("[A-Za-z0-9:-]+");
+
     if (!StringUtils.hasText(resourceId)) {
       throw new StatusException(Status.INVALID_ARGUMENT.withDescription("resourceId is required"));
     } else if (!resourceValidation.matcher(resourceId).matches()) {
