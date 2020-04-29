07ccdf5efcc95b1214b9356c4ffbe5bc5b02ab58
Geoff Bourne <geoff.bourne@rackspace.com>
2020-04-29 14:18:21 -0500

    Added caching of ZoneApiClient.getAvailableZones

--- src/main/java/com/rackspace/salus/telemetry/ambassador/config/RestClientsConfig.java
+++ src/main/java/com/rackspace/salus/telemetry/ambassador/config/RestClientsConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Rackspace US, Inc.
+ * Copyright 2020 Rackspace US, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@ package com.rackspace.salus.telemetry.ambassador.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rackspace.salus.acm.web.client.AgentInstallApi;
 import com.rackspace.salus.acm.web.client.AgentInstallApiClient;
+import com.rackspace.salus.monitor_management.config.ZoneApiCacheConfig;
 import com.rackspace.salus.monitor_management.web.client.MonitorApi;
 import com.rackspace.salus.monitor_management.web.client.MonitorApiClient;
 import com.rackspace.salus.monitor_management.web.client.ZoneApi;
@@ -29,8 +30,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import(ZoneApiCacheConfig.class)
 public class RestClientsConfig {
 
   private final ServicesProperties servicesProperties;
